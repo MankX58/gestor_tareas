b531362ad47a3c2c99d0ac1e3953410b9cacac73
Manuel <manuelguvi500@gmail.com>
2025-03-09 20:59:24 -0500

    Diagrama de clases

--- src/gestor/usuario.py
+++ src/gestor/usuario.py
@@ -2,7 +2,6 @@ class Usuario:
     def __init__(self, nombre, contraseña) -> None:
         self.nombre = nombre
         self.contraseña = contraseña
-        self.tareas = []
 
     def cambiar_contraseña(self, nueva_contraseña) -> None:
         self.contraseña = nueva_contraseña
@@ -10,12 +9,3 @@ class Usuario:
     def verificar_contraseña(self, contraseña) -> bool:
         return self.contraseña == contraseña
     
-    def agregar_tarea(self, tarea) -> None:
-        self.tareas.append(tarea)
-        
-    def ver_tareas(self) -> list:
-        for i, tarea in enumerate(self.tareas, start=1):
-            print(f"\nTAREA {i}\n-------------------\n{tarea}\n-------------------")
-        
-    def eliminar_tarea(self, nombre) -> None:
-        self.tareas = [tarea for tarea in self.tareas if tarea.nombre != nombre]
